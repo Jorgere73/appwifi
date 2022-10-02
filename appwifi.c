637ca4cd4bc5bd3d2285dfa9d6e3977422392edc
PatataAcustica <100472783@alumnos.uc3m.es>
2022-10-02 12:26:21 +0200

    Emilio collect

--- appwifi.c
+++ appwifi.c
@@ -2,6 +2,7 @@
 #include <ctype.h> //Incluye función tolower()
 #include <stdbool.h>
 #include <string.h>
+#include "appwifi.h"
 
 int printmenu()
 {
@@ -40,12 +41,16 @@ bool wificollector_quit()
 
 }
 
-void wificollector_collect()
+void wificollector_collect()//tenemos q abrir los archivos y recolectar lo q tiene dentro en un array
 {
 
 bool exit2;
 bool salir;
 char decision;
+char *cadena[MAX_VALUE];
+//char cadena[80];
+int cont=0;
+
 do
 {
   do
@@ -65,18 +70,25 @@ do
       fcelda = fopen(celda, "r");
       if(fcelda == NULL) {printf("Error al cargar el archivo\n");}
       else
-      {
-        while(!feof(fcelda)) //Mientras no haya llegado al final del archivo...
-        {
-          printf("%c", fgetc(fcelda));
-        }
+      {    
+        do{//para meter el contenido del archivo en un char [][]
+          int leido=fgetc(fcelda);
+           if(!feof(fcelda)){
+              char a =(char) leido;//pasamos el int a char
+              strcat(cadena[cont],  a);
+              printf("%s",a);
+              
+            }
+           } while (!feof(fcelda));
+          //printf("La cadena es: %s\n", cadena);
+          cont++;
       }
       exit2=true;
     }else {
       printf("Introduce un número valido. ");
       exit2=false;
     }
-    fclose(fcelda);
+     fclose(fcelda);
   }while(exit2==false);
   printf("¿Desea añadir otro punto de acceso? [S/N]\n");
   scanf("%s", &decision); decision = tolower(decision);
@@ -84,4 +96,35 @@ do
   else if(decision == 'n') {salir= false;}
   else {printf("Introduzca una de las opciones dadas [S/N]\n");}
   } while (salir==true);
+}
+
+/*void wificollector_show_data_one_network(){
+  int informacion;
+  char celda[]="info_cell_";
+  char celda2[21];
+  FILE *fcelda;
+  printf("Indique el ESSID (entre ""): \n");
+  scanf("%d\n",&informacion);
+  if(informacion>=1 && informacion<=21)
+  {
+    sprintf(celda2,"%u",informacion);//concatenamos para tener la celda elegida
+    strcat(celda,celda2);
+    strcat(celda,".txt");
+    printf("%s\n", celda);
+    fcelda = fopen(celda, "r");
+
+  }else{
+    printf("Introduce un número valido. ");
+
+  }
+
+}*/
+
+void wificollector_display(){
+
+
+printf("¿indique el número de la celda que desea conocer su información (1-21): \n?")
+
+
+
 }
