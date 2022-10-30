b50ca5653fd84ce94b409d6a7e9a44f22c0f5a6c
PatataAcustica <100472783@alumnos.uc3m.es>
2022-10-30 14:32:10 +0100

    Created splitstring() prototype

--- selectorbestworst.c
+++ selectorbestworst.c
@@ -7,13 +7,34 @@ void wificollector_select_best(int** source)
 
 }
 
-int searchinstring(int* str, int size)
+int returnquality(int* str, int size)
 {
+    char* ptr;
     char* string = calloc(size, sizeof(char));
     for(int i = 0; i < size; i++)
     {
         string[i] = str[i] + '0';
     }
     
+    
     return 0;
+}
+
+char* splitstring(char* str, char div)
+{
+    char* sstring; = strtok(str, div);
+    char** substr = malloc(sizeof(char*)*5);
+    int numsstr = 0:
+    for(int i = 0; i < 5; i++)
+    {
+        substr[i] = calloc(sizeof(char*)*MAX_VALUE/3);
+    }
+
+    while(sstring != NULL)
+    {
+        substr[numsstr] = sstring;
+        sstring = strtok(NULL, div);
+        numsstr++;
+    }
+    return substr;
 }
