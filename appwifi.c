7e9d4b66407dea65de13bafe52eb03c0f7c4e7cd
PatataAcustica <100472783@alumnos.uc3m.es>
2022-12-06 15:22:34 +0100

    a

--- appwifi.c
+++ appwifi.c
@@ -1,171 +1,140 @@
 #include <stdio.h>
-#include <stdlib.h> //malloc()
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
-#include "appwifi.h"
 
-#define MAX_VALUE 500
+/*ME CREO MI LISTA
+cada nodo va a tener una wifi diferente no va a tener un archivo .cell*/
 
-int** wificollector_collect()//tenemos q abrir los archivos y recolectar lo q tiene dentro en un array
+struct nodo
 {
-  bool exit2;
-  int** collect;
-  bool nosalir;
-  char decision;
-  int* cadena = (int*)calloc(sizeof(int*),MAX_VALUE);
+    char *info;
+    struct nodo *prox;
+};
+typedef struct nodo lista;
 
-  collect = (int**) calloc(sizeof(int*),22);
-  for(int it = 0; it <= 22; it++)
-  {
-    collect[it] = (int*)calloc(sizeof(int),MAX_VALUE);
-  }
+//para crear nodos y meter la información dentro
+struct nodo *crear_nodo(char *info1)
+{
+    struct nodo *nodo_nuevo = (struct nodo *)malloc(sizeof(struct nodo));
 
-  do
-  {
+    nodo_nuevo->info = info1;
+    nodo_nuevo->prox = NULL;
+
+    return nodo_nuevo;
+}
+lista *wificollector_collect()
+{
+    lista *milista = calloc(sizeof(lista *), 48); //Reservo memoria para cada posición de cada wifi 1wifi 1nodo.
+
+    for (int it = 0; it <= 48; it++)
+    {
+        milista->info = (char *)calloc(sizeof(int), 500);
+    }
+
+    bool exit2;
+    bool nosalir;
+    char decision;
+    //mi collec==milista
+
+    char *infoA = (char *)calloc(sizeof(int), 500);
+
     do
     {
-      int eleccion2;
-      char celda[]="cells/info_cell_";
-      char celda2[21];
-      int tamano = 0;
-      FILE *fcelda; //Archivo que contendrá la celda elegida
-      printf("¿Qué celda desea recolectar? (1-21): \n");
-      scanf("%d",&eleccion2);
-      if(eleccion2>=1 && eleccion2<=21)
-      {
-        sprintf(celda2,"%u",eleccion2); //Concatenamos para tener la celda elegida
-        strcat(celda,celda2);
-        strcat(celda,".txt");
-        fcelda = fopen(celda, "r"); //Abrimos archivo en modo lectura
-
-        tamano = filesize(fcelda);
-        collect[eleccion2] = realloc(collect[eleccion2], sizeof(int*)*tamano); //Cambia el tamano de la celda al del archivo
-        collect[22][eleccion2] = tamano;
-        //cadena = realloc(cadena, sizeof(int*)*tamano); //Cambia el tamano de cadena al del archivo en cuestion
-
-        if(fcelda == NULL) {printf("Error al cargar el archivo\n");}
-        else
-        {    
-          for(int cont = 0; !feof(fcelda); cont++)
-          {
-            int leido = fgetc(fcelda);
-            cadena[cont] = leido; //Leemos archivo, y transferimos contenido a cadena
-          }
-        }
-
-        for(int i = 0; i < MAX_VALUE; i++)
+        do
         {
-          collect[eleccion2][i] = cadena[i]; //De cadena, transferimos el contenido a la matriz collect
-        } 
-        collect[0][eleccion2] = 1;
-        exit2=true;
-      }else {
-        printf("Introduce un número valido.\n");
-        exit2=false;
-        continue;
-      }
-      fclose(fcelda);
-    }while(exit2==false);
-    printf("¿Desea añadir otro punto de acceso? [S/N]\n");
-    scanf("%s", &decision); decision = tolower(decision);
-    if(decision == 's') {nosalir= true;}
-    else if(decision == 'n') {nosalir= false;}
-    else {printf("Introduzca una de las opciones dadas [S/N]\n");}
+            int eleccion2;
+            char celda[] = "cells/info_cell_";
+            char celda2[21];
+
+            FILE *fcelda; //Archivo que contendrá la celda elegida
+            printf("¿Qué celda desea recolectar? (1-21): \n");
+            scanf("%d", &eleccion2);
+            if (eleccion2 >= 1 && eleccion2 <= 21)
+            {
+                sprintf(celda2, "%u", eleccion2); //Concatenamos para tener la celda elegida
+                strcat(celda, celda2);
+                strcat(celda, ".txt");
+                //printf("%s", celda);
+                fcelda = fopen(celda, "r"); //Abrimos archivo en modo lectura
+                if (fcelda == NULL)
+                {
+                    printf("Error al cargar el archivo\n");
+                }
+                else
+                {
+
+                    for (int cont = 0; !feof(fcelda); cont++)
+                    {
+                      
+                        int leido = fgetc(fcelda);
+                        infoA[cont] = leido; //Leemos archivo, y transferimos contenido a cadena
+                        printf("Impresión numero %d:\n%s", cont, infoA);
+                    }
+                    printf("Estoy vacio:\ndd%s", infoA);
+                    milista->prox = crear_nodo(infoA); //creamos el nodo y metemos la información de las wifis
+                                                       // printf("Estoy vacio:\ndd%s", infoA);
+                    /*COMPROBAMOS QUE ESTAMOS METIENDO BIEN LA INFORMACION
+                    printf("tonto");
+                    while (infoA != NULL)
+                    {
+                        printf("%s", infoA);
+                    }*/
+                }
+                exit2 = true;
+            }
+            else
+            {
+                printf("Introduce un número valido.\n");
+                exit2 = false;
+                continue;
+            }
+            fclose(fcelda);
+        } while (exit2 == false);
+        printf("¿Desea añadir otro punto de acceso? [S/N]\n");
+        scanf("%s", &decision);
+        //decision = tolower(decision);
+        if (decision == 's' || decision == 'S')
+        {
+            nosalir = true;
+            milista = milista->prox;
+        }
+        else if (decision == 'n' || decision == 'N')
+        {
+            nosalir = false;
+        }
+        else
+        {
+            printf("Introduzca una de las opciones dadas [S/N]\n");
+        }
     } while (nosalir);
+    return milista;
+}
+
+/*
+
+Celda 1: 3 wifis.
+Celda 2: 3 wifis
+Celda 3: 3 wifis
+Celda 4: 3 wifis.
+Celda 5: 3 wifis
+Celda 6: 3 wifis.
     
-    free(cadena);//Liberamos memoria
-    return collect;
-}
+Celda 7: 2 wifis.
+Celda 8: 2 wifis
+Celda 9: 2 wifis
+Celda 10: 2 wifis.
+Celda 11: 2 wifis
+Celda 12: 2 wifis
+Celda 13: 2 wifis.
+Celda 14: 2 wifis
+Celda 15: 2 wifis
+Celda 16: 2 wifis.
+Celda 17: 2 wifis
+Celda 18: 2 wifis
+Celda 19: 2 wifis.
+Celda 20: 2 wifis
+Celda 21: 1 wifis
 
-/*void wificollector_show_data_one_network(){
-  int informacion;
-  char celda[]="info_cell_";
-  char celda2[21];
-  FILE *fcelda;
-  printf("Indique el ESSID (entre ""): \n");
-  scanf("%d\n",&informacion);
-  if(informacion>=1 && informacion<=21)
-  {
-    sprintf(celda2,"%u",informacion);//concatenamos para tener la celda elegida
-    strcat(celda,celda2);
-    strcat(celda,".txt");
-    printf("%s\n", celda);
-    fcelda = fopen(celda, "r");
-
-  }else{
-    printf("Introduce un número valido. ");
-
-  }
-
-}*/
-
-void wificollector_display(int** celdas)
-{
-  char continuadisplay = '0';
-  do
-  {
-    int numc; //Número de la celda de la cual queremos imprimir información
-    printf("Indique el número de la celda del que desea conocer su información (1-21): \n");
-    scanf("%d", &numc);
-    if(celdas[0][numc] == 0) {printf("La celda que has seleccionado está vacía, elija otra\n");}
-    else 
-    {
-      for(int c = 0; c < MAX_VALUE; c++)
-      { 
-        printf("%c", celdas[numc][c]);
-      }
-      printf("\n");
-    }
-    printf("¿Desea imprimir información de otra celda? [S/N]");
-    getchar();
-    scanf("%c", &continuadisplay);
-    printf("\n");
-    continuadisplay = tolower(continuadisplay);
-  }while(continuadisplay == 's');
-}
-
-void wificollector_display_all(int** celdas)
-{
-  for(int i = 1; i <= 21; i++)
-  {
-    if(celdas[0][i] == 0) {continue;} //Si está vacía, pasamos a la siguiente
-    else
-    {
-      printf("Celda %d:\n\n", i);
-      for(int c = 0; c < MAX_VALUE; c++)
-      {
-        printf("%c", celdas[i][c]);
-      }
-      printf("\n\n");
-    }
-  }
-}
-
-//----------------------------FUNCIONES PROPIAS-----------------------------
-
-void freearraymem(int** arr)
-{
-  for(int c = 0; c <= 22; c++)
-  {
-    free(arr[c]);
-  }
-  free(arr);
-}
-
-void aumentartamano(int** arr, int tamano)
-{
-  for(int i = 0; i <= 21; i++)
-  {
-    arr[i] = realloc(arr[i], tamano);
-  }
-}
-
-int filesize(FILE* file)
-{
-  int sz;
-  fseek(file, 0L, SEEK_END); //Va hasta final del archivo y retorna posicion, así sabemos el tamaño
-  sz = ftell(file);
-  rewind(file);
-  return sz;
-}
-
+En total tenemos 48 wifis difrentes
+*/
